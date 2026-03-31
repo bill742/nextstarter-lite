aac5936493f53cee2c57a9e35f512fdc2d48305f
Bill Dean <742designs@gmail.com>
2026-03-31 18:08:16 -0400

    Add fix as suggested by react doctor.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -22,7 +22,7 @@ const Header = () => {
       setIsScrolled(window.scrollY > 20);
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll, { passive: true });
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
